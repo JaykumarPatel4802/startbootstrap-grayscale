14fff6959398c2de5cae0c9b78302e7c9812ecea
Jaykumar Patel <jaykumar.patel4802@gmail.com>
2019-03-28 21:40:13 +0000

    Database Update

--- flask_app1.py
+++ flask_app1.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 app.static_folder = 'static'
+app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def main():
@@ -21,3 +24,23 @@ def map():
 def login():
     # found in ../templates/
     return render_template("login.html")
+
+# @app.route('/subscribe', methods=['POST'])
+# def subscribe():
+#     # found in ../templates/
+#     email = request.form['inputEmail']
+#     with open('./subscribe.csv') as f:
+#         f.write(email + "\n")
+#     return render_template("login.html")
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{JaykumarPatel4802}:{MySQLDataBase}@{JaykumarPatel4802.mysql.pythonanywhere-services.com}/{TrackUrTransit}".format(
+    username="JaykumarPatel480",
+    password="MySQLDataBase",
+    hostname="JaykumarPatel4802.mysql.pythonanywhere-services.com",
+    databasename="TrackUrTransit",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
