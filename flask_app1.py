203a877709e71e2d5418bc810edec435788c72ad
Jaykumar Patel <jaykumar.patel4802@gmail.com>
2019-04-10 04:13:09 +0000

    This is my first comment for the database

--- flask_app1.py
+++ flask_app1.py
@@ -11,11 +11,12 @@ SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostnam
     hostname="JaykumarPatel4802.mysql.pythonanywhere-services.com",
     databasename="JaykumarPatel480$TrackUrTransit",
 )
-app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+
+db.Comments
 
 class Comment(db.Model):
 
@@ -24,19 +25,43 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4096))
 
-comments = []
+
+if __name__ == '__main__':
+    app.run()
 
 @app.route('/', methods=['GET'])
 def main():
     # found in ../templates/
     return render_template("index.html")
 
-@app.route('/search/', methods=['GET'])
+@app.route('/search', methods=['GET'])
 def search():
     # found in ../templates/
     return render_template("search_page.html")
+
+@app.route('/create_email', methods=["GET", "POST"])
+def create_email(request):
+    # SQL to update database with STATIC info
+    # db.Comments.update("static_email_for_now@gmail.com")
+
+    #comment = Comment(content="test_email@gmail.com")
+    # db.session.add(comment)
+    # db.session.commit()
+    #
+    #
+    # found in ../templates/
+    if request.method == "GET":
+        return render_template("index.html", comments=Comment.query.all())
+
+    comment = Comment(content=request.form["contents"])
+    db.session.add(comment)
+    db.session.commit()
+    return redirect(url_for("index"))
 
 @app.route('/map/', methods=['GET'])
 def map():
     # found in ../templates/
     return render_template("map.html")
+
+
+# https://blog.pythonanywhere.com/121/
